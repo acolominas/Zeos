45d533b5bbe225de45783018b02a279e9002d185
Arnau <acolominasilla@gmail.com>
2017-10-24 00:01:26 +0200

    Update sched.h

--- include/sched.h
+++ include/sched.h
@@ -14,7 +14,8 @@
 
 enum state_t { ST_RUN, ST_READY, ST_BLOCKED };
 
-struct task_struct {
+struct task_struct {  
+  unsigned long kernel_esp;
   int PID;			/* Process ID. This MUST be the first field of the struct. */
   page_table_entry * dir_pages_baseAddr;
   struct list_head list;
